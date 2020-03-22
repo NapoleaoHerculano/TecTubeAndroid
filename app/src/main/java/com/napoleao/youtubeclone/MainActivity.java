bff63b6ff979d8fe9aa241c6bf9815dd2651080d
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-03-22 14:11:25 -0300

    Adding API support library, API key, and required permissions.

--- app/src/main/java/com/napoleao/youtubeclone/MainActivity.java
+++ app/src/main/java/com/napoleao/youtubeclone/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String GOOGLE_YOUTUBE_API_KEY = "AIzaSyAC24Tw4U0viHh7tNqVQOvesyEHpGfeanc";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
