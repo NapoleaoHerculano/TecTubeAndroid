7b656f08d50ffe05c3ed7654c3e45287cbe73358
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-03-22 14:56:15 -0300

    Configuring player and playing video

--- app/src/main/java/com/napoleao/youtubeclone/MainActivity.java
+++ app/src/main/java/com/napoleao/youtubeclone/MainActivity.java
@@ -3,14 +3,40 @@ package com.napoleao.youtubeclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class MainActivity extends YouTubeBaseActivity
+            implements YouTubePlayer.OnInitializedListener {
+
+    private YouTubePlayerView youTubePlayerView;
     private static final String GOOGLE_YOUTUBE_API_KEY = "AIzaSyAC24Tw4U0viHh7tNqVQOvesyEHpGfeanc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.viewYouTubePlayer);
+        youTubePlayerView.initialize(GOOGLE_YOUTUBE_API_KEY, this);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        Toast.makeText(this, "Sucesso ao iniciar o player!", Toast.LENGTH_SHORT).show();
+
+        if( !wasRestored ){
+            youTubePlayer.cueVideo("RIaJHh60hQY");
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, "Erro ao iniciar o player!", Toast.LENGTH_SHORT).show();
     }
 }
