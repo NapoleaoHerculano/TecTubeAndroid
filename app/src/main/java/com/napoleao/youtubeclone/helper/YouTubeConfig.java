5763d4cf4b81afc4dad6f944185a009eb3561672
NapoleaoHerculano <francivaldo.napoleao@gmail.com>
2020-03-22 22:08:08 -0300

    API access configuration class

---
+++ app/src/main/java/com/napoleao/youtubeclone/helper/YouTubeConfig.java
@@ -0,0 +1,10 @@
+package com.napoleao.youtubeclone.helper;
+
+/**
+ * Nesta classe são definidos os itens necessários para o acesso da API pelo Retrofit.
+ */
+public class YouTubeConfig {
+    public static String GOOGLE_YOUTUBE_API_KEY = "YOUR API KEY HERE";
+    public static String CANAL_ID = "UCdmGjywrxeOPfC7vDllmSgQ";
+    public static String URL_BASE = "https://www.googleapis.com/youtube/v3/";
+}
